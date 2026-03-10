471ac8617842be36dd0d3ba985ea558bc5cff159
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-03-10 18:48:02 +0500

    Add files via upload

---
+++ 01 Basics/04. Student data .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    string name = "Ali Khan";
+     string gender = "Male";
+    int age = 20;
+    int rollno = 35;
+    double cgpa = 3.6;
+    bool passed = true;
+    
+    cout<<"Student Name : "<<name <<endl;
+    cout<<"Gender : "<<gender<<endl;
+    cout<<"Age : "<<age<<endl;
+    cout<<"Roll No : "<<rollno<<endl;
+    cout<<"Passed : "<<passed;
+    
+    return 0;
+   
+}
