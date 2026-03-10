2f3a044308305227666e75da053f9962cb01ada7
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-03-10 08:17:14 +0500

    Add files via upload

---
+++ 01 Basics/03. Data types .cpp
@@ -0,0 +1,15 @@
+// A program that shows what type of data is stored  by each data type.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age = 20;
+    float height = 5.7;
+    char grade = 'A';
+    
+    cout<<" Age : "<<age<<endl;
+    cout<<" Height : "<<height <<endl;
+    cout<<" Grade : "<<grade;
+    
+    return 0;
+}
