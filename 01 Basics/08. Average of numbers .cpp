d630fd927001549d760e2baa1e840d458e5fc032
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-03-11 18:12:38 +0500

    Add files via upload

---
+++ 01 Basics/08. Average of numbers .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ( )
+{
+    int a , b , c ;
+    float average ;
+    cout<<"Enter numbers : ";
+    cin>>a>>b>>c;
+    
+    average =(a+ b+ c) /3 ; 
+    cout<<"Average : "<<average; 
+    
+    return 0;
+    
+}
